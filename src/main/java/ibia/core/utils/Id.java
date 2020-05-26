299b31dab66302ddf12090682bd92e04e3aedd0c
quantomistro <mahmoodsaifi38@gmail.com>
2020-05-27 03:34:29 +0800

    Implement some methods for utils.Id

--- src/main/java/ibia/core/utils/Id.java
+++ src/main/java/ibia/core/utils/Id.java
@@ -1,6 +1,39 @@
 package ibia.core.utils;
 
+import java.util.Date;
+
 public class Id {
-    // static util methods for dealing with ids
-    // e.g generating ids, getting "created" date from id (since ids use timestamps) etc.
+    public static String generate(String type) throws Exception {
+        String ts = Long.toString(System.currentTimeMillis());
+        switch (type.toLowerCase()) {
+            case "committee":
+            case "com":
+                return "COM" + ts;
+            case "conference":
+            case "con":
+                return "CON" + ts;
+            case "delegate":
+            case "del":
+                return "DEL" + ts;
+            case "entity":
+            case "ent":
+                return "ENT" + ts;
+            default:
+                throw new Exception("Invalid ID type provided.");
+        }
+    }
+
+    public static Date createdAt(String id) throws Exception {
+        if (verify(id)) {
+            Long ts = Long.parseLong(id.substring(3));
+            return new Date(ts);
+        } else {
+            throw new Exception("Invalid ID provided.");
+        }
+    }
+
+    public static Boolean verify(String id) {
+        // handle verification
+        return true;
+    }
 }
