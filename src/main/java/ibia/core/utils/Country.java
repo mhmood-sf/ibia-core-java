4832a1dc9ee97c2d00f9e420c503f1c3fc7da12c
quantomistro <mahmoodsaifi38@gmail.com>
2020-05-30 05:38:04 +0800

    Write some comments for utils.Country

--- src/main/java/ibia/core/utils/Country.java
+++ src/main/java/ibia/core/utils/Country.java
@@ -1,20 +1,23 @@
 package ibia.core.utils;
 
-// TODO: Instead of hardcoding countries like this, pull the data from a remote source
-// like this: https://github.com/stefangabos/world_countries/blob/master/data/en/world.json
-// and maybe have a packaged version of it in the jar, and then for updating it, pull the data from
-// the repo when an internet connection is available. That, or have a button to let the user choose to update it.
-// In this file, add a search function for finding a country from the world.json file, and return its data
-// create a function searchCountry() that returns a Country instance.
-
-// country icons, names, codes, etc.
+/*
+ * Utility class for dealing with country names and flags.
+ * All methods are static.
+ * All data is obtained from the repository at: https://github.com/stefangabos/world_countries
+ */
 public class Country {
-    // all countries by full name
+    /*
+     * Returns list of the full names for all 249 territories
+     * that have an officially assigned ISO 3166-1 code.
+     */
     public static String[] listbyName() {
         return new String[0];
     }
 
-    // all countries by alpha-2 ISO codes
+    /*
+     * Returns list of the alpha-2 codes for all 249 territories
+     * that have an officially assigned ISO 3166-1 code.
+     */
     public static String[] listByCode() {
         return new String[0];
     }
@@ -26,18 +29,26 @@ public class Country {
     }
 */
 
-    // get full name from alpha-2 code
+    /*
+     * Return the full name for a territory,
+     * given its alpha-2 code.
+     */
     public static String codeToName(String code) {
         return "";
     }
 
-    // get alpha-2 code from name
+    /*
+     * Returns the alpha-2 code for a territory,
+     * given its full name.
+     */
     public static String nameToCode(String name) {
         return "";
     }
 
-    // get path to country flag from alpha-2 code
-    // the flag should work with the FXMLLoader in Util.
+    /*
+     * Returns the (relative) path to a .png file for
+     * the flag of a territory, given its alpha-2 code.
+     */
     public static String flagPath(String code) {
         return "";
     }
