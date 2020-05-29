ba33c9ec180eb481779309b0a578f644ea7c9cce
quantomistro <mahmoodsaifi38@gmail.com>
2020-05-30 05:39:03 +0800

    Create test class for DbClient

---
+++ src/test/java/ibia/core/db/DbClientTest.java
@@ -0,0 +1,15 @@
+package ibia.core.db;
+
+import org.junit.Test;
+
+import ibia.core.entities.Committee;
+
+public class DbClientTest {
+    protected void setUp() throws Exception {
+        System.out.println("this was executed");
+    }
+
+    @Test public void testInsert() throws Exception {
+        DbClient.insert(new Committee());
+    }
+}
