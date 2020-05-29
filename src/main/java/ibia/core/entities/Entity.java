29f38886b83014a9ad2501c30d823764c86b8837
quantomistro <mahmoodsaifi38@gmail.com>
2020-05-30 05:37:42 +0800

    Update Entity interface

--- src/main/java/ibia/core/entities/Entity.java
+++ src/main/java/ibia/core/entities/Entity.java
@@ -1,5 +1,18 @@
 package ibia.core.entities;
 
-interface Entity {
-    // fill this in eventually
+/*
+ * An interface representing any generic entity
+ * that posesses a certain type and ID and represents
+ * an aspect of a(n) MUN conference.
+ */
+public interface Entity {
+    // get the type of entity
+    public String getType();
+
+    // GETTERS and SETTERS used by hibernate
+    public String getId();
+    public void setId(String id);
+
+    public String getName();
+    public void setName(String name);
 }
