ca85f89437f6fd0de6e2516487e1917f3caffd05
quantomistro <mahmoodsaifi38@gmail.com>
2020-05-30 05:09:29 +0800

    implemented DbClient.insert + boilerplate

--- src/main/java/ibia/core/db/DbClient.java
+++ src/main/java/ibia/core/db/DbClient.java
@@ -1,5 +1,78 @@
 package ibia.core.db;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import ibia.core.entities.Entity;
+
+/*
+ * Handles all database-related operations.
+ * All methods are static.
+ * Uses an embedded H2 database under the hood,
+ * through the Hibernate ORM.
+ */
 public class DbClient {
-    // handle db ops, including caching etc.
+    private static StandardServiceRegistry registry;
+    private static SessionFactory sessionFactory;
+
+    private static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                // Create registry
+                registry = new StandardServiceRegistryBuilder().configure().build();
+
+                // Create MetadataSources
+                MetadataSources sources = new MetadataSources(registry);
+
+                // Create Metadata
+                Metadata metadata = sources.getMetadataBuilder().build();
+
+                // Create SessionFactory
+                sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                if (registry != null) {
+                    StandardServiceRegistryBuilder.destroy(registry);
+                }
+
+                throw e;
+            }
+        }
+        return sessionFactory;
+    }
+
+    // TODO: test this
+    public static void insert(Entity entity) {
+        SessionFactory sf = getSessionFactory();
+        Session session = sf.openSession();
+        session.beginTransaction();
+        session.save(entity);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    // TODO: unimplemented
+    public static void update() {
+    }
+
+    // TODO: unimplemented
+    public static void delete() {
+
+    }
+
+    // TODO: unimplemented
+    public static void find() {
+
+    }
+
+    public static void shutdown() {
+        if (registry != null) {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
 }
