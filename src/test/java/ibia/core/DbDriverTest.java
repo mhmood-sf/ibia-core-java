cfbeee92fc997377ec873d68afbc3bbcaaf60af0
quantomistro <mahmoodsaifi38@gmail.com>
2020-07-16 00:31:59 +0800

    Clean up test

--- src/test/java/ibia/core/DbDriverTest.java
+++ src/test/java/ibia/core/DbDriverTest.java
@@ -1,5 +1,7 @@
 package ibia.core;
 
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
 
 import ibia.core.entities.Committee;
@@ -7,14 +9,9 @@ import ibia.core.entities.Committee;
 public class DbDriverTest {
     protected Committee com = new Committee("it's all objects?", 10, "always has been");
 
-    protected void setUp() throws Exception {
-        System.out.println("this was executed");
-    }
-
     @Test public void testInsertAndFind() throws Exception {
         DbDriver.insertOne(com);
         Committee found = DbDriver.findOne(Committee.class, com.getId());
-        System.out.println(found);
+        assertNotNull(found);
     }
-
 }
