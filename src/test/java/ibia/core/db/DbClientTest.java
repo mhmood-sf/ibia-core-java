1045d5528003d32b4e8bfeb4be4d446ab73225b1
quantomistro <mahmoodsaifi38@gmail.com>
2020-07-15 21:08:37 +0800

    Update method names for DbClient in DbClientTest

--- src/test/java/ibia/core/db/DbClientTest.java
+++ src/test/java/ibia/core/db/DbClientTest.java
@@ -5,11 +5,18 @@ import org.junit.Test;
 import ibia.core.entities.Committee;
 
 public class DbClientTest {
+    protected Committee com = new Committee("it's all objects?", 10, "always has been");
+
+    public DbClientTest() throws Exception {}
+
     protected void setUp() throws Exception {
         System.out.println("this was executed");
     }
 
-    @Test public void testInsert() throws Exception {
-        DbClient.insert(new Committee("it's all objects?", 10, "always has been"));
+    @Test public void testInsertAndFind() throws Exception {
+        DbClient.insertOne(com);
+        Committee found = DbClient.findOne(Committee.class, com.getId());
+        System.out.println(found);
     }
+
 }
