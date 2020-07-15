a4f0a7382f72e49127f83b63a549ef633dc6b331
quantomistro <mahmoodsaifi38@gmail.com>
2020-07-16 00:31:49 +0800

    ibia.core.Client WIP

--- src/main/java/ibia/core/Client.java
+++ src/main/java/ibia/core/Client.java
@@ -4,14 +4,26 @@ import java.util.ArrayList;
 
 import ibia.core.entities.Committee;
 import ibia.core.entities.Conference;
+import ibia.core.entities.Delegate;
 
 public class Client {
     public static void beginNewConference(String name, int size, int committeeSize) {
         Conference conf = new Conference(name, size);
         ArrayList<Committee> committees = new ArrayList<>();
+        ArrayList<Delegate> delegates = new ArrayList<>();
 
         for (int i = 0; i <= size; i++) {
-            committees.add(new Committee(i + "", committeeSize, conf.getId()));
+            Committee com = new Committee(i + "", committeeSize, conf.getId());
+            committees.add(com);
+
+            for (int j = 0; j <= committeeSize; j++) {
+                Delegate del = new Delegate(j + "", com.getId());
+                delegates.add(del);
+            }
         }
+
+        DbDriver.insertOne(conf);
+        DbDriver.insertAll(committees);
+        DbDriver.insertAll(delegates);
     }
 }
