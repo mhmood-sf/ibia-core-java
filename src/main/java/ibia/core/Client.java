d3516c7b0cf54c136adb3a5333399030a8aca852
quantomistro <mahmoodsaifi38@gmail.com>
2020-07-19 08:09:46 +0800

    [Client] dont create lotsof entities automatically (in beginNewConference)

--- src/main/java/ibia/core/Client.java
+++ src/main/java/ibia/core/Client.java
@@ -13,25 +13,9 @@ public class Client {
      * the conference instance, as well as the committees and
      * delegates. Returns the conference instance created.
      */
-    public static Conference beginNewConference(String name, int size, int committeeSize) {
-        Conference conf = new Conference(name, size);
-        ArrayList<Committee> committees = new ArrayList<>();
-        ArrayList<Delegate> delegates = new ArrayList<>();
-
-        for (int i = 0; i <= size; i++) {
-            Committee com = new Committee(i + "", committeeSize, conf.getId());
-            committees.add(com);
-
-            for (int j = 0; j <= committeeSize; j++) {
-                Delegate del = new Delegate(j + "", "Drumpf", com.getId());
-                delegates.add(del);
-            }
-        }
-
+    public static Conference beginNewConference(String name) {
+        Conference conf = new Conference(name);
         DbDriver.insertOne(conf);
-        DbDriver.insertAll(committees);
-        DbDriver.insertAll(delegates);
-
         return conf;
     }
 
