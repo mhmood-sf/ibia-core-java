e0c99a3e41fadf1d0f7ae8930c4802382ec096d8
quantomistro <mahmoodsaifi38@gmail.com>
2020-10-23 12:42:01 +0800

    Create log file if it doesn't exist, and append to log file instead of rewriting it every time.

--- src/main/java/ibia/core/Log.java
+++ src/main/java/ibia/core/Log.java
@@ -1,5 +1,6 @@
 package ibia.core;
 
+import java.io.File;
 import java.util.logging.FileHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
@@ -16,16 +17,24 @@ public class Log {
     public static Logger getLogger() {
         if (logger == null) {
             logger = Logger.getLogger("");
-            // for efficiency, do not log to console.
-            logger.setUseParentHandlers(false);
+            logger.setUseParentHandlers(false); // do not log to parents, for efficiency
+
             try {
+                // Create the file if it doesn't exist
+                File file = new File("data/ibia.log");
+                if (!file.isFile()) file.createNewFile();
+
+                // Set the formatter and add handler to logger
                 SimpleFormatter fmt = new SimpleFormatter();
-                handler = new FileHandler("data/ibia.log");
+                handler = new FileHandler("data/ibia.log", true);
                 handler.setFormatter(fmt);
                 logger.addHandler(handler);
+
+                // Indicate a new log session is starting.
+                info("===== SESSION START =====");
             } catch (Exception e) {
-                System.out.println("[ibia | WARN] Failed to access log file (ibia.log).");
-                System.out.println("[ibia | WARN] All log messages will be printed to console for this session.");
+                System.out.println("WARN: [ibia] Failed to access log file (ibia.log).");
+                System.out.println("WARN: [ibia] All log messages will be printed to console for this session.");
             }
         }
 
